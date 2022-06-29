0d50751edd079b65e24dbb783a9248257c912587
Tdiaz5 <48680360+Tdiaz5@users.noreply.github.com>
2022-06-30 00:10:49 +0200

    script that will hopefully format the root data

---
+++ view_data.py
@@ -0,0 +1,16 @@
+import ROOT
+import matplotlib.pyplot as plt
+
+filename = ""
+f = ROOT.TFile.Open(filename, "READ")
+tree = f.Get("DepositionGeant4")
+hist1 = tree.Get("detection_time_scintillator_box1")
+hist2 = tree.Get("detection_time_scintillator_box2")
+
+nbins = 1000
+
+all_events_1 = [[hist1.GetBinContent(x, y) for x in range(nbins)] for y in range(nbins)]
+all_events_2 = [[hist2.GetBinContent(x, y) for x in range(nbins)] for y in range(nbins)]
+
+plt.plot(all_events_1[0])
+plt.show()
