cbf29b5bf781463208c9f5ff748e9e67801dcc52
tdiaz5 <tobiasdiazdenhollander@live.nl>
2022-06-30 10:45:19 +0200

    fixed wrong binning

--- convert_TH2D.py
+++ convert_TH2D.py
@@ -9,8 +9,10 @@ hist2 = tree.Get("detection_time_scintillator_box2")
 
 # format to python lists
 nbins = 1000
-all_events_1 = [[hist1.GetBinContent(x, y) for x in range(nbins)] for y in range(nbins)]
-all_events_2 = [[hist2.GetBinContent(x, y) for x in range(nbins)] for y in range(nbins)]
+n_events = 10
+
+all_events_1 = [[hist1.GetBinContent(x, y) for x in range(nbins)] for y in range(n_events)]
+all_events_2 = [[hist2.GetBinContent(x, y) for x in range(nbins)] for y in range(n_events)]
 
 with open('scintillator_simulations.pkl', 'w') as f:
     pickle.dump([all_events_1, all_events_2], f)
