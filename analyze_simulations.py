a7f0cd1b67e2c574df0043da2e5eab8c6e13901c
tdiaz5 <tobiasdiazdenhollander@live.nl>
2022-06-30 13:33:58 +0200

    first decay events

--- analyze_simulations.py
+++ analyze_simulations.py
@@ -1,17 +1,34 @@
 import pickle
+from tkinter import N
 import matplotlib.pyplot as plt
 
-filename = "scintillator_simulations.pkl"
+filename = "1000events_1MeV.pkl"
 
 f = open(filename, 'rb')
 all_events_1, all_events_2 = pickle.load(f)
 f.close()
 
+def rebin(events, factor):
+    new_events = []
+    index = 0
+
+    while index < len(events):
+        new_bin = 0
+
+        for i in range(factor):
+            new_bin += events[index]
+            index += 1
+        
+        new_events.append(new_bin)
+
+    return new_events
+
+events_1_rebinned = [rebin(event, 20) for event in all_events_1]
+print(len(events_1_rebinned[0]))
+
 n_events = len(all_events_1)
 
 for i in range(len(all_events_1)):
-    plt.plot(all_events_1[i])
+    plt.bar([i for i in range(len(events_1_rebinned[i]))], events_1_rebinned[i])
     plt.title(f"Event {i}/{n_events}")
-    plt.draw()
-    plt.pause(0.1)
-    plt.clf()
+    plt.show()
