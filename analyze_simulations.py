304595fbe22bdb15573e9b07736dc49cc9df327c
tdiaz5 <tobiasdiazdenhollander@live.nl>
2022-07-07 16:34:01 +0200

    plot upgrade

--- analyze_simulations.py
+++ analyze_simulations.py
@@ -2,9 +2,12 @@ import pickle
 from tkinter import N
 import matplotlib.pyplot as plt
 
-filename = "1000events_1MeV.pkl"
+filename = "1000events_1MeV_realscale"
 
-f = open(filename, 'rb')
+path = "data_files/"
+format = ".pkl"
+
+f = open(path + filename + format, 'rb')
 all_events_1, all_events_2 = pickle.load(f)
 f.close()
 
@@ -24,11 +27,16 @@ def rebin(events, factor):
     return new_events
 
 events_1_rebinned = [rebin(event, 20) for event in all_events_1]
-print(len(events_1_rebinned[0]))
-
 n_events = len(all_events_1)
 
+# with open(path + filename + '_rebinned' + format, 'wb') as f:
+#     pickle.dump(events_1_rebinned, f)
+
 for i in range(len(all_events_1)):
-    plt.bar([i for i in range(len(events_1_rebinned[i]))], events_1_rebinned[i])
-    plt.title(f"Event {i}/{n_events}")
+    plt.bar([4 * i for i in range(len(events_1_rebinned[i]))], events_1_rebinned[i], width=4, align="center")
+    # plt.title(f"Event {i}/{n_events}")
+    plt.title("Simulation of a 1 MeV muon detection")
+    plt.xlim(0, 200)
+    plt.xlabel("time (ns)")
+    plt.ylabel("photon count")
     plt.show()
