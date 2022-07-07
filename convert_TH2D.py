47f768a62beba428f1829318341cfff2925a85b2
tdiaz5 <tobiasdiazdenhollander@live.nl>
2022-07-07 16:44:28 +0200

    added program description

--- convert_TH2D.py
+++ convert_TH2D.py
@@ -1,7 +1,12 @@
 import ROOT
 import pickle
 
+# ------------- PARAMETERS TO EDIT -----------------
 filename = "modules.root"
+nbins = 1000
+n_events = 10
+# --------------------------------------------------
+
 
 f = ROOT.TFile.Open(filename, "READ")
 tree = f.Get("DepositionGeant4")
@@ -9,9 +14,6 @@ hist1 = tree.Get("detection_time_scintillator_box1")
 hist2 = tree.Get("detection_time_scintillator_box2")
 
 # format to python lists
-nbins = 1000
-n_events = 10
-
 all_events_1 = [[hist1.GetBinContent(x, y) for x in range(nbins)] for y in range(n_events)]
 all_events_2 = [[hist2.GetBinContent(x, y) for x in range(nbins)] for y in range(n_events)]
 
